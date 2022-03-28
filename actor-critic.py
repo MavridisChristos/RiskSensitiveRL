f46ee63943ff570c2e7ca3e3bd74b708e125166b
Christos Mavridis <68886651+MavridisChristos@users.noreply.github.com>
2022-03-28 18:12:36 -0400

    Update

--- actor-critic.py
+++ actor-critic.py
@@ -16,18 +16,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm 
 
+import gym
 import torch  
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.distributions import Categorical
 
-import cartpole
-
 plt.ioff() # turn off interactive mode: only show figures with plt.show()
 plt.close('all')
 
 #%% Hyper-parameters 
+
+name = 'Online-Risk'
 
 # Risk Sensitivity
 risk_objective = 'beta'
@@ -39,8 +40,8 @@ risk_beta = -0.1
 
 # Online or Batch
 online = True
-online = False
-beta00 = 10
+# online = False
+alpha = 1
 update_buffer = 1001 # update actor-critic buffer for offline
 
 # Baseline
@@ -50,40 +51,54 @@ value_approximation = True
 value_approximation = False
 
 # Simulation Epochs
-train_loops = 7 # hundreds of training epochs
+train_loops = 11 # hundreds of training epochs
 test_loops = 0 # hundreds of testing epochs
 nepochs = 100
 time_steps = 200 # time horizon for successful epoch
 
 # Neural Networks
 multiple_layers = False
-nn_hidden_size = 16
-lr = 0.01
+nn_hidden_size = 256
+lr = 0.001
 gammaAC = 0.99
+
+# Game
+game='cartpole'
+# game='acrobot'
+# game='mountaincar'
 
 # plot folder
 dfolder = './plots'
 os.makedirs(dfolder, exist_ok=True)
 
 # save results file
-os.makedirs('./results', exist_ok=True)
-results_file = './results/last_results.pkl'
-
-# Plot
-plot_figures = False
+results_folder = './results/'+game
+os.makedirs(results_folder, exist_ok=True)
+results_file = results_folder+'/'+name+'.pkl'
+save_to_file = True
+# save_to_file = False
 
 # Animation
 show_animation = False
-
-# Save results to file
-save_to_file = False
 
 # random seed
 rs=0
 
 #%% Environment Initialization and Random Seeds
 
-env = cartpole.CartPoleEnv()
+if game=='cartpole':
+    # import cartpole
+    # env = cartpole.CartPoleEnv()
+    env = gym.make("CartPole-v1")
+elif game=='acrobot':
+    # import acrobot
+    # env = acrobot.AcrobotEnv()
+    env = gym.make("Acrobot-v1")
+elif game=='mountaincar':
+    # import mountaincar
+    # env = mountaincar.MountainCarEnv()
+    env = gym.make("MountainCar-v0")
+    
 env_seed=env.seed(rs)
 env.action_space.np_random.seed(rs)
 np.random.seed(rs)
@@ -247,18 +262,20 @@ def online_update_actor_critic(new_state,rewards,values,log_probs,beta):
     else:
         advantage = returns + gammaAC * new_value - values
         
-    actor_loss = -beta*(log_probs * values.detach()).mean()
+    actor_loss = -(log_probs * values.detach()).mean()
     sign = 1
     if risk_objective=='beta' or risk_objective=='betainverse' or risk_objective=='sign':
         sign = -np.sign(risk_beta)
     else:
         sign = -1
-    critic_loss = sign*beta*advantage.pow(2).mean()
+    critic_loss = sign*advantage.pow(2).mean()
 
+    a_optimizer.param_groups[0]["lr"] = beta
     a_optimizer.zero_grad()
     actor_loss.backward()
     a_optimizer.step()
     
+    c_optimizer.param_groups[0]["lr"] = beta
     c_optimizer.zero_grad()
     critic_loss.backward()
     c_optimizer.step()
@@ -272,17 +289,19 @@ training_std=[]
 for k in range(train_loops):
     avg = 0
     std2 = 0
+    
+    lr = lr * 0.95
+        
     # for nepochs epochs (episodes)
     for i in range(nepochs):
         
-        beta0 = beta00/(i+1)
-        beta0 = beta00
+        score = 0
         log_probs = []
         values = []
         rewards = []
         entropy = 0
         perfect=True
-  
+        
         # reset/observe current state
         state = env.reset()
         state = torch.FloatTensor(state).to(device)
@@ -305,11 +324,12 @@ for k in range(train_loops):
             values.append(value)
             rewards.append(torch.tensor([reward], dtype=torch.float, device=device))
             log_probs.append(log_prob)
-            # perfect = not done
+            score+= reward
             
             # Batch or Online Update
             if online:
-                online_update_actor_critic(new_state, rewards, values, log_probs, beta0*0.99)
+                stepsize = lr*alpha/(t+1)
+                online_update_actor_critic(new_state, rewards, values, log_probs, stepsize)
                 log_probs = []
                 values = []
                 rewards = []
@@ -332,68 +352,23 @@ for k in range(train_loops):
             update_actor_critic(new_state, rewards, values, log_probs, perfect)
         
         # compute average over time_steps repeats 
-        training_all.append(t+1)
-        avg = avg + 1/(i+1) * (t+1-avg) # = avg * i/(i+1) + (t+1)/(i+1) 
+        training_all.append(score)
+        avg = avg + 1/(i+1) * (score-avg) # = avg * i/(i+1) + (t+1)/(i+1) 
         if i==0:
-            std2 = (t+1 - avg)**2 
+            std2 = (score - avg)**2 
         else:
-            std2 = (i-1)/i * std2 + 1/(i+1) * (t+1 - avg)**2             
+            std2 = (i-1)/i * std2 + 1/(i+1) * (score - avg)**2             
         
     # Compute Average number of timesteps
     training_avg.append(avg)
     training_std.append(np.sqrt(std2))
     print(f'{k+1}-th episode: Average timesteps: {avg}')
-        
-    # Visualize Value Function
-    # if lowD and plot_figures:    
-    #     vis_V(actor,critic)
-    #     plt.show()
-
-#%% Testing Loop
-
-# time_steps=1000    
-testing_avg=[]
-avg = 0
-
-for k in range(test_loops):
-
-    for i in range(nepochs):
-        
-        # reset/observe current state
-        state = env.reset()
-        state = torch.FloatTensor(state).to(device)
-        
-        # repeat until failure and up to time_steps
-        for t in range(time_steps):
-            
-            # pick next action
-            value = critic(state)
-            policy = actor(state)
-            action = policy.sample()
-            # policy_np = policy.detach().numpy() 
-            # action = np.argmax(np.squeeze(policy_np))
-            
-            # observe new state
-            new_state, reward, done, info = env.step(action.cpu().numpy())
-            new_state = torch.FloatTensor(new_state).to(device) 
-            
-            state=new_state
-            if done:
-                # print("Episode finished after {} timesteps".format(t+1))
-                break
-        
-        # compute average over 100 repeats    
-        avg = avg + 1/(i+1) * (t+1-avg) # = avg * i/(i+1) + (t+1)/(i+1)   
-        
-    update_once=False
-    testing_avg.append(avg)
-    print(f'Testing: Average timesteps: {avg}.')
     
 #%% Save results to file 
 
 if save_to_file:    
 
-    my_results = [training_avg, testing_avg]
+    my_results = [training_all, training_avg]
                
     if results_file != '':
         with open(results_file, mode='wb') as file:
@@ -411,31 +386,11 @@ x=(np.arange(len(training_avg))+1)*nepochs
 x=np.insert(x, 0, 1)
 y=np.array(training_avg)
 y=np.insert(y,0,training_all[0])
-plt.plot(x,y, label='Training Average',color='b',linewidth=2)
+plt.plot(x,y, label='Average Reward',color='b',linewidth=2)
 plt.xlabel('Number of episodes')
-plt.ylabel('Timesteps')
+plt.ylabel('Reward')
 plt.legend()
 plt.show()
-
-#%% Plot Training Curve
-    
-# fig = plt.figure(facecolor='white')
-
-# plt.title('Training Curve')
-# x=np.arange(len(training_avg))+1
-# y=np.array(training_avg)
-# sigma=np.array(training_std)
-# plt.plot(x,y, label='Training Averages')
-# plt.fill(np.concatenate([x, x[::-1]]),
-#           np.concatenate([y - 1.9600 * sigma,
-#                         (y + 1.9600 * sigma)[::-1]]),
-#           alpha=.2, fc='b', ec='None', label='95%')
-# # plt.plot(len(training_avg)+np.zeros(len(testing_avg))+1,testing_avg,'r*',
-# #                   label='Testing Averages')
-# plt.xlabel('Hundreds of episodes')
-# plt.ylabel('Average number of timesteps')
-# plt.legend()
-# plt.show()
     
 #%% Animation
 
@@ -443,6 +398,7 @@ if show_animation:
     
     time_steps = 1000
     avg = 0
+    score = 0
     
     for i in range(1):
         
@@ -462,64 +418,14 @@ if show_animation:
             # observe new state
             new_state, reward, done, info = env.step(action.cpu().numpy())
             new_state = torch.FloatTensor(new_state).to(device) 
+            score += reward
             
             state=new_state
             if done:
-                print("Episode finished after {} timesteps".format(t+1))
+                print("Episode finished. Reward: {}".format(t+1))
                 break
         
-        avg = avg + 1/(i+1) * (t+1-avg)
+        avg = avg + 1/(i+1) * (score-avg)
         
-    print(f'Average Number of timesteps: {avg}')
+    print(f'Average Reward: {avg}')
     env.close()
-
-#%% Plot Initial State Space
-
-def vis_V(actor,critic):
-    
-    three_D = False
-    
-    # defining x, y, z co-ordinates 
-    mesh_points = 101
-    box2 = [-2.5,2.5]
-    box3 = [-2.5,2.5]
-    s2 = np.linspace(-box2, box2, mesh_points)
-    s3 = np.linspace(-box3, box3, mesh_points)
-    S2, S3 = np.meshgrid(s2, s3)
-    
-    # Plot the surface.
-    V_plot = np.zeros((mesh_points,mesh_points))
-    for i in range(mesh_points):
-        for j in range(mesh_points):
-            state = np.array([s2[i],s3[j]])
-            V_plot[i,j]= critic(state)
-      
-    if three_D:
-        # create a new figure for plotting 
-        fig, ax = plt.subplots(facecolor='white',subplot_kw={"projection": "3d"})
-        
-        surf = ax.plot_surface(S2, S3, V_plot, cmap=cm.coolwarm,
-                                linewidth=0, antialiased=False)
-        # Customize x,y axis
-        ax.tick_params(axis='both', which='major', labelsize=8)
-        # ax.set_xlim(-box2, box2)
-        ax.set_xlabel(r'$\theta$')
-        # ax.set_ylim(-box3, box3)
-        ax.set_ylabel(r'$\dot\theta$')
-        ax.set_zlabel(r'$V(\theta,\dot\theta)$')
-        
-        ax.view_init(50, 50)
-        # Add a color bar which maps values to colors.
-        fig.colorbar(surf, shrink=0.3, aspect=5, pad=0.0)
-
-    else:
-        
-        fig, ax = plt.subplots(1,1)
-        img = ax.imshow(V_plot,extent=[-1,1,-1,1],cmap=cm.coolwarm)
-        x_label_list = [-2.5, 0, 2.5]
-        ax.set_xticks([-1, 0, 1])
-        ax.set_xticklabels(x_label_list)
-        y_label_list = [-2.5, 0, 2.5]
-        ax.set_yticks([-1, 0, 1])
-        ax.set_yticklabels(y_label_list)
-        fig.colorbar(img,shrink=0.3, aspect=5, pad=0.01)
