668e61ff1551965ccdc98e631b9739df73d2ebdc
Christos Mavridis <68886651+MavridisChristos@users.noreply.github.com>
2023-03-09 12:00:50 -0500

    Update rsrl-run.py

--- RiskSensitiveRL/rsrl-run.py
+++ RiskSensitiveRL/rsrl-run.py
@@ -40,8 +40,8 @@ def run_all_seeds(
                 cut_lr=False,
                 # Model Variations
                 model_var = [-0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3],
-                verbose = 1,
-                rs=43):
+                verbose = 0,
+                rs=0):
     
     for rs in [0,1,2,3,4,5,6,7,8,9]:
         
